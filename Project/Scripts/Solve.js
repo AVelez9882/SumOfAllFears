cd2853cdcfed68fcb3520b9104cea8ad86b168a5
Angelica Velez <avelez9882@gmail.com>
2020-07-16 09:27:18 -0400

    Spacing
    corrected alignment

--- Project/Scripts/Solve.js
+++ Project/Scripts/Solve.js
@@ -1,38 +1,38 @@
 ﻿<script>
 
-    var input = document.getElementById("sumInput");
-            input.addEventListener("keyup", function (event) {
-                if (event.keyCode === 13) {
-        event.preventDefault();
-                    document.getElementById("sumBtn").click();
+        var input = document.getElementById("sumInput");
+        input.addEventListener("keyup", function (event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                document.getElementById("sumBtn").click();
+            }
+        });
+
+		$("#soafOut").hide();
+
+        $("#sumBtn").on("click", function () {
+            let array = [10, 15, 3, 7];
+            let k = parseInt($("#sumInput").val());
+            let message = "";
+            for (let index = 0; index < array.length - 1; index++) {
+                //If any number in the array is larger than K we can ignore it
+                if (array[index] > k)
+                    continue;
+
+                let solution = k - array[index]; //if K = 17 and number = 7 we have to find 10
+
+                if (array.indexOf(solution, index + 1) > -1) {
+                    message = `A solution was found for a K value of ${k}.`;
+                    break;
                 }
-            });
-
-			$("#soafOut").hide();
-
-            $("#sumBtn").on("click", function () {
-        let array = [10, 15, 3, 7];
-                let k = parseInt($("#sumInput").val());
-                let message = "";
-                for (let index = 0; index < array.length - 1; index++) {
-                    //If any number in the array is larger than K we can ignore it
-                    if (array[index] > k)
-                        continue;
-
-                    let solution = k - array[index]; //if K = 17 and number = 7 we have to find 10
-
-                    if (array.indexOf(solution, index + 1) > -1) {
-        message = `A solution was found for a K value of ${k}.`;
-                        break;
-                    }
-                    else {
-        message = `No solution for a K value of ${k} was found`;
-                    }
+                else {
+                    message = `No solution for a K value of ${k} was found`;
                 }
-                //Print result to user
-                $("#soafOut").text(message);
-                $("#soafOut").show();
-            });
+            }
+            //Print result to user
+            $("#soafOut").text(message);
+            $("#soafOut").show();
+        });
 
 </script>
 
